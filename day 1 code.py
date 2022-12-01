6eb30b3923212ef30d339d212c02f4e8bad3083e
Alexander Waterston <alexanderwaterston@Alexanders-MacBook-Pro.local>
2022-12-01 12:01:33 +0000

    dirty one liner

--- day 1 code.py
+++ day 1 code.py
@@ -1,2 +1,5 @@
 print(max([sum([int(y) for y in x.split()]) for x in open('day 1 input.txt').read().split("\n\n")]))
-print(sum(sorted(([sum([int(y) for y in x.split()]) for x in open('day 1 input.txt').read().split("\n\n")]))[-3:]))
+print((sorted(([sum([int(y) for y in x.split()]) for x in open('day 1 input.txt').read().split("\n\n")]))[:-4:-1]))
+
+#in one (dirty) line
+a,b,c = sorted(([sum([int(y) for y in x.split()]) for x in open('day 1 input.txt').read().split("\n\n")]))[:-4:-1]; print(f"{a}, {a+b+c}")
