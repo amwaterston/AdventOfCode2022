8ef87184df0985dc2d92ae12e52b777ae8a5b522
Alexander Waterston <alexanderwaterston@Alexanders-MBP.youfibre.com>
2022-12-02 11:19:47 +0000

    day 2 part 2

--- day 2 code.py
+++ day 2 code.py
@@ -1,4 +1,4 @@
 #part 1 in one line
-print(sum([({0:3, -2: 6, 1: 6, -1: 0, 2: 0}[(ord(r[2]) - ord('X')) - (ord(r[0]) - ord('A'))] + (1 + ord(r[2]) - ord('X'))) for r in open('day 2 input.txt').read().split("\n")]))
+print(sum([({0:3, -2: 6, 1: 6, -1: 0, 2: 0}[(x := ord(r[2]) - ord('X')) - (ord(r[0]) - ord('A'))] + (1 + x)) for r in open('day 2 input.txt').read().split("\n")]))
 print(sum([((ord(r[0]) - ord('A') + ((w := ord(r[2]) - ord('X')) -1)) % 3) + 1 + (w * 3) for r in open('day 2 input.txt').read().split("\n")]))
 
